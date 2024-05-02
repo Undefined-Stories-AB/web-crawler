8715cf3021569719d6d196c1e548c388855ab019
Joaqim Planstedt <gpg@joaqim.xyz>
2024-05-02 13:28:38 +0200

    Release candidate 2

--- crawler.py
+++ crawler.py
@@ -30,9 +30,12 @@ def process_product_pages(url, driver: webdriver.Chrome, entries: list = []) -> 
     product_links = driver.find_elements(
         By.CSS_SELECTOR, PRODUCT_LINK_CSS_SELECTOR)
 
+    product_links_count = len(product_links)
+
     # Iterate through each product link
-    for link in product_links:
+    for link in product_links[len(entries):]:
         product_url = link.get_attribute('href')
+
         if product_url:
             # Visit each product page
             driver.get(product_url)
@@ -42,54 +45,58 @@ def process_product_pages(url, driver: webdriver.Chrome, entries: list = []) -> 
                 By.CSS_SELECTOR,
                 SUGGESTED_STOCK_AMOUNT_CSS_SELECTOR).get_attribute(SUGGESTED_STOCK_AMOUNT_ATTRIBUTE_NAME)
 
-            antal_input = driver.find_element(
-                By.CSS_SELECTOR,
-                INPUT_PURCHASE_AMOUNT_CSS_SELECTOR)
-
-            # Set value of order amount to 999
-            driver.execute_script(
-                "arguments[0].value = arguments[1];", antal_input, 999)
-
-            # Optionally, you can trigger a JavaScript change event to simulate user interaction
-            driver.execute_script(
-                "arguments[0].dispatchEvent(new Event('change', { bubbles: true }));", antal_input)
-
-            # Click on buy button
-            submit_button = driver.find_element(
-                By.CSS_SELECTOR,
-                PURCHASE_SUBMIT_CSS_SELECTOR
-            )
-
             stock_amount = suggested_stock_amount
             msg = ''
-
-            submit_button.submit()
-
             try:
-                # Wait for the redirection to complete (adjust timeout as needed)
-                # WebDriverWait(driver, 10).until( EC.url_changes(driver.current_url))
+                antal_input = driver.find_element(
+                    By.CSS_SELECTOR,
+                    INPUT_PURCHASE_AMOUNT_CSS_SELECTOR)
 
-                # Once redirected, get the text content of the desired div
-                div_element = WebDriverWait(driver, 10).until(
-                    EC.presence_of_element_located((By.CLASS_NAME, "Error"))
+                # Set value of order amount to 999
+                driver.execute_script(
+                    "arguments[0].value = arguments[1];", antal_input, 999)
+
+                # Optionally, you can trigger a JavaScript change event to simulate user interaction
+                driver.execute_script(
+                    "arguments[0].dispatchEvent(new Event('change', { bubbles: true }));", antal_input)
+
+                # Click on buy button
+                submit_button = driver.find_element(
+                    By.CSS_SELECTOR,
+                    PURCHASE_SUBMIT_CSS_SELECTOR
                 )
-                # msg = div_element.text
-                try:
-                    stock_amount = re.search(r"(\d+)", msg).group(0)
+
+                # This will redirect us to a simple Error page
+                submit_button.submit()
+
+                # Get the text content of the Error div
+                error_message = WebDriverWait(driver, 10).until(
+                    EC.presence_of_element_located(
+                        (By.CLASS_NAME, "Error"))
+                ).text
+
+                matched_stock_amount = re.search(
+                    r".*([-+]?\d+).*", error_message)
+                if matched_stock_amount:
+                    stock_amount = matched_stock_amount.group(0)
                     msg = "Confirmed"
-                except:
-                    pass
             except TimeoutException:
                 msg = f"Unconfirmed. Assumed stock amount is: {suggested_stock_amount}"
 
+            product = str(product_url.split('/')[-1])
+
             entry = {
                 # "url": str(product_url),
-                "product": str(product_url.split('/')[-1]),
+                "product": product,
                 "stock_amount": stock_amount,
                 "suggested_stock_amount": suggested_stock_amount,
                 "msg": msg
             }
             entries.append(entry)
+
+            if len(entries) >= product_links_count:
+                break
+            return process_product_pages(url, driver, entries)
 
     return entries
 
@@ -107,9 +114,8 @@ if __name__ == "__main__":
         with open("stocks.json", "a", encoding="utf-8") as fp:
             entries = []
             for url in URLS:
-                process_product_pages(url, chrome_driver, entries)
+                entries.append(process_product_pages(url, chrome_driver))
             fp.write(json.dumps(entries))
-
     except:
         raise Exception("Failed to process product pages")
     finally:
