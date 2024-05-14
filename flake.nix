098cd689473d3b2389b0796092197e3e27b3d2f9
Joaqim Planstedt <gpg@joaqim.xyz>
2024-05-14 10:55:01 +0200

    Activate Cronjob for 8AM and 8PM
    skip-checks:true

--- flake.nix
+++ flake.nix
@@ -3,23 +3,22 @@
     mach-nix.url = "mach-nix/3.5.0";
   };
 
-  outputs = {self, nixpkgs, mach-nix }@inp:
+  outputs = { self, nixpkgs, mach-nix }@inp:
     let
       l = nixpkgs.lib // builtins;
       supportedSystems = [ "x86_64-linux" ];
       forAllSystems = f: l.genAttrs supportedSystems
-        (system: f system (import nixpkgs {inherit system;}));
+        (system: f system (import nixpkgs { inherit system; }));
     in
     {
       defaultPackage = forAllSystems (system: pkgs: mach-nix.lib."${system}".mkPython {
         requirements = ''
-		      python-dotenv
-    		  requests
-		      selenium
-		      feedgen
-          dateutils
-          datetime
-	      '';
+                    python-dotenv
+                    requests
+                    selenium
+                    dateutils
+                    datetime
+        '';
         packagesExtra = [
           pkgs.chromium
           pkgs.chromedriver
